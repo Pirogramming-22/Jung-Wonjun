00502d4909f64ca34b79ae8d14dbc4c57a83a1fd
dietken1 <dietken1@ajou.ac.kr>
2025-01-02 22:18:42 +0900

    stage 6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,39 @@
 num = 0
 count = 0
-while True:
-    inputVal = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    if not inputVal.isdigit():
-        print("정수를 입력하세요")
-        continue
-    elif int(inputVal) > 3 or int(inputVal) < 1:
-        print("1,2,3 중 하나를 입력하세요")
-        continue
-    else:
-        count = int(inputVal)
-        break
-
-for _ in range(count):
-    num+=1
-    print(f"playerA : {num}")
 
 while True:
-    inputVal = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    if not inputVal.isdigit():
-        print("정수를 입력하세요")
-        continue
-    elif int(inputVal) > 3 or int(inputVal) < 1:
-        print("1,2,3 중 하나를 입력하세요")
-        continue
-    else:
-        count = int(inputVal)
-        break
+    while True:
+        inputVal = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        if not inputVal.isdigit():
+            print("정수를 입력하세요")
+            continue
+        elif int(inputVal) > 3 or int(inputVal) < 1:
+            print("1,2,3 중 하나를 입력하세요")
+            continue
+        else:
+            count = int(inputVal)
+            break
 
-for _ in range(count):
-    num+=1
-    print(f"playerB : {num}")
+    for _ in range(count):
+        num+=1
+        print(f"playerA : {num}")
+        if num == 31:
+            exit()
+
+    while True:
+        inputVal = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        if not inputVal.isdigit():
+            print("정수를 입력하세요")
+            continue
+        elif int(inputVal) > 3 or int(inputVal) < 1:
+            print("1,2,3 중 하나를 입력하세요")
+            continue
+        else:
+            count = int(inputVal)
+            break
+
+    for _ in range(count):
+        num+=1
+        print(f"playerB : {num}")
+        if num == 31:
+            exit()
