e458197842020ead932419b4fb9cb7fe8311500f
dietken1 <dietken1@ajou.ac.kr>
2025-01-07 16:38:32 +0900

    설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dietk\Desktop\Piro22\Jung-Wonjun\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
