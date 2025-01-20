220f46fe14400262c58f621c198c1cf0b98f0e89
dietken1 <dietken1@ajou.ac.kr>
2025-01-20 22:02:57 +0900

    기능 보완 및 README 작성 완료

--- piro22_Ajax_Pirostagram/comment/urls.py
+++ piro22_Ajax_Pirostagram/comment/urls.py
@@ -5,4 +5,5 @@ app_name = 'comment'
 
 urlpatterns = [
     path('<int:post_id>/comment/', views.comment, name='comment'),
+    path('<int:post_id>/delete/<int:pk>', views.delete, name='delete'),
 ]
--- piro22_Ajax_Pirostagram/comment/views.py
+++ piro22_Ajax_Pirostagram/comment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from post.models import Post
+from comment.models import Comment
 from .forms import Commentform
 
 def comment(request, post_id):
@@ -23,3 +24,8 @@ def comment(request, post_id):
         'comments': comments,
     }
     return render(request, 'comment/comment.html', context)
+
+def delete(request, post_id, pk):
+    comment = Comment.objects.get(id=pk)
+    comment.delete()
+    return redirect('comment:comment', post_id=post_id)
